2947be3bde327e7b8da8e46056b3d759b3317e22
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-25 10:49:37 +0530

    Add project files.

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LambdaExpression;
+
+Console.WriteLine("Hello, World!");
+List<Person> listPersonInCity = new List<Person>();
+LambdaDemo.AddRecords(listPersonInCity);
+//Retrieving_TopTwoRecord_ForAgels_LessThanSixty(listPersonInCity);
+LambdaDemo.CheckingForTeenagerPerson(listPersonInCity);
+LambdaDemo.AllPersonsAverageAge(listPersonInCity);
+LambdaDemo.CheckNameExistOrNot(listPersonInCity);
+Console.ReadKey();
